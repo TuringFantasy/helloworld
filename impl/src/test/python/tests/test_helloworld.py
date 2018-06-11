6aa5bef9e75e213e66c77d75c2ec0d321c248a9e
ravjangacfx <40008396+ravjangacfx@users.noreply.github.com>
2018-06-11 00:34:08 -0700

    Added a simple python test for Unit Test.

---
+++ impl/src/test/python/tests/test_helloworld.py
@@ -0,0 +1,20 @@
+import os
+from APIGateway import *
+
+HELLO_WORLD_SERVICE="service/io.zolontech.services/helloworld/1.0.0/"
+class TestUM:
+    def setup(self):
+	print("Initiating API Gateway Module")
+	self.apigateway = APIGateway(host=os.getenv('MACAW_PLATFORM_URL'),
+					access_key="5979ce4d-2700-4170-bc0b-608b665f73c0",
+					secret_key="f729e369-7dea-47cb-9cd8-590dd3d51f20",
+					login_method="access-key")
+	self.apigateway.login()
+ 
+    def teardown(self):
+	self.apigateway.logout()
+ 
+    def test_numbers_5_6(self):
+	response = self.apigateway.Post(HELLO_WORLD_SERVICE+"sayHello", payload=None)
+	assert (response is not None), "Response is not received"
+	assert (response.json() == "Hello, World!"), "Not expected hello is received"
